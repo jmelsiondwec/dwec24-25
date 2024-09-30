16faa44acf6e1932f91decb48c7ff5f3f78cd301
jmelsiondwec <jmelsion@iesjoanramis.org>
2024-09-30 15:52:35 +0200

    Final UD02

---
+++ UD02 - Valors, tipus i operadors/script.js
@@ -0,0 +1,96 @@
+// NÚMEROS
+
+// let a = 10;
+// a = 5;
+// let b = 9.98;
+// let c = 3.768e6; // 3768000
+// const pi = 3.141592;
+//pi = 9;
+//console.log(pi);
+
+// Aritmètica
+//console.log(100 + 4 * 11);
+//console.log(1 - 4 / 2);
+//console.log(9 % 3);
+
+// Números especials
+// Infiniy i -Infinity
+//console.log(-Infinity);
+// NaN
+//console.log(NaN);
+//console.log(100 / 0);
+
+// CADENES (STRING)
+// console.log('hola');
+// console.log('dobles');
+// console.log(`accents`);
+
+// Salt de linia
+//console.log("Salt\nde\n\"linia\"");
+//let cadena = "nar";
+//console.log("con" + "ca" + "te" + cadena);
+
+// template literals
+//let calcul = 100 / 2;
+//console.log(`la meitat de 100 és ${calcul}`);
+
+// OPERADORS UNARIS
+//console.log(typeof(4.5));
+//console.log(typeof "x");
+//console.log(typeof calcul);
+
+// BINARIS -> + i * 
+//console.log(7 - 5);
+//console.log(-5);
+
+// - és unari i binari (un poc Schrondinger)
+
+// BOOLEAN
+// true i false
+
+//console.log(3 > 2);
+//console.log(3 < 2);
+
+//console.log("Azozaiza" > "Zoriquete");
+
+//console.log("hola" == "Hola");
+//console.log(!("hola" != "Hola"));
+//console.log(5 === "5");
+
+//console.log(NaN == (Infinity - Infinity));
+
+// OPERADORES LÓGICOS
+// AND (i) OR (o) NOT (negació)
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false || false);
+
+// OPERADOR TERNARI
+//console.log((6 > 3) ? "és true" : "és false");
+
+// VALORS BUITS
+// null, undefined
+//console.log(undefined && (4 < 6) ? 1 : 2);
+
+//console.log(8 * undefined);
+
+// let b;
+// console.log(b);
+
+// CONVERSIÓ AUTOMÀTICA DE TIPUS
+//console.log(8*undefined);
+//console.log(typeof ("5" + 1));
+//console.log(1 + "5");
+//console.log("5" - 1);
+//console.log(null == 0);
+
+// Cortocircuito de los operadores && y ||
+//console.log(null || "joan");
+//console.log("pepe" || "joan");
+
+console.log(0 || 100);
+console.log(0 && 100);
+
+// ?? -> ||
+console.log(0 ?? 100);
+console.log(null ?? 100);
