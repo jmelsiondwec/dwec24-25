78350245d2964b18a2653c8c55dc0c7ed44f42d7
jmelsiondwec <jmelsion@iesjoanramis.org>
2024-10-14 17:19:40 +0200

    Inici UD05

---
+++ UD05 - Estructures de dades - Objectes i Arrays/Arrays/script.js
@@ -0,0 +1,150 @@
+// let numeros = [1, 2, 3, 4]; // 4 elementos
+// console.log(numeros[0]); // indexados de 0 hasta su longitud - 1
+// console.log(numeros[3]); // último
+// console.log(numeros[4]); // undefined
+// // length: longitud = número de elementos del array
+// console.log(numeros[numeros.length - 1]);
+
+// let nombres = ["pepe", "María", 'Joan', `Pere`];
+// console.log(nombres[0]);
+// console.log(nombres[1]);
+// console.log(nombres[2]);
+// console.log(nombres[3]);
+
+// let mescla1 = ["Hola", 10, true];
+// console.log(typeof mescla1[0]);
+// console.log(typeof mescla1[1]);
+// console.log(typeof mescla1[2]);
+
+// console.log(mescla1);
+
+// let numeros = new Array();
+// numeros[0] = 10;
+// numeros[1] = 20;
+// numeros[3] = 10;
+// console.log(numeros);
+
+// let noms = new Array('María', 'Joan');
+// console.log(noms);
+
+// let numeros = new Array();
+// numeros = Array(5).fill('Hola');
+// console.log(numeros);
+
+// Métodos de Array
+// push(): Agrega uno o más elementos al final del array
+ const frutas = ['manzana', 'platano', 'naranja'];
+// console.log(frutas);
+// frutas.push('uva', 'pera');
+// console.log(frutas);
+
+// pop(): Elimina el último elemento del array.
+// let uva = frutas.pop();
+// console.log(uva);
+// console.log(frutas);
+
+// shift(): Elimina el primer elemeto del array.
+// let primer = frutas.shift();
+// console.log(frutas);
+// console.log(primer);
+
+// unshift(): Agregar uno o més elementos al inicio del array.
+// frutas.unshift('melon', 'sandia');
+// console.log(frutas);
+
+// concat(): Combina dos o más arrays y retorna un nuevo array.
+// const frutas2 = ['melocoton', 'kiwi'];
+// const combinacion = frutas.concat(frutas2);
+// console.log(combinacion);
+
+// slice(): Devuelve una copia una superficial de una porción del array.
+// frutas.push('kiwi', 'melon', 'sandia', 'melocoton');
+// console.log(frutas);
+// const frutas2 = frutas.slice(1, 5);
+// console.log(frutas2);
+
+// splice(): Cambia el contenido de un array eliminado,
+// reemplazando o agregando elementos.
+// frutas2.splice(1, 1, 'uva', 'tomate');
+// console.log(frutas2);
+
+// indexOf(): Devuelve el primer índice en el que se encuentra
+// un elemento dado en un array o -1 si no lo encuentra.
+//const indiceElementoBuscado = frutas.indexOf('Platano'.toLowerCase());
+//console.log(indiceElementoBuscado);
+
+// join(): Une todos los elementos de un array en una cadena,
+// utilizando un separador especificado.
+// const unionElementos = frutas.join('*');
+// console.log(unionElementos);
+
+// sort(): Ordena los elementos de un array alfabéticamente (si
+// son cadenas) o numéricamente (para números).
+//frutas.sort();
+//console.log(frutas);
+
+// Ordena por valores unicode!!
+// const numeros = [10, 5, 8, 3, 1];
+// console.log(numeros);
+// numeros.sort();
+// console.log(numeros);
+
+// const ciudades = ['Zaragoza', 'madrid', 'Barcelona'];
+// ciudades.sort();
+// console.log(ciudades);
+
+// Afortunadamente sort() tiene un parámetro opcional
+//const numeros = [10, 5, 8, 3, 1];
+// console.log(numeros.sort(function(a, b) {
+//   // -1 a < b
+//   // 1 a > b
+//   // 0 a == b
+//   return a - b;
+// }));
+
+//console.log(numeros.sort((a, b) => a - b));
+
+//const ciudades = ['Zaragoza', 'madrid', 'Barcelona', 'Ávila'];
+// ciudades.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 :
+//                         a.toLowerCase() < b.toLowerCase() ? -1 : 0);
+// console.log(ciudades);
+
+// ciudades.sort((a, b) => a.localeCompare(b));
+// console.log(ciudades);
+
+// const ciudades = ['Zaragoza', 'madrid', 'Barcelona', 'Ávila'];
+// for(let i = 0; i < ciudades.length; i++) {
+//   console.log(ciudades[i]);
+// }
+// for(let ciudad in ciudades) {
+//   console.log(ciudad);
+// }
+// for(let ciudad of ciudades) {
+//   console.log(ciudad);
+// }
+// ciudades.forEach(ciudad => {
+//   console.log(ciudad.toUpperCase())
+// });
+
+// Desestructuración
+//const numeros = [1, 2, 3];
+// const [a, b, c] = numeros;
+// console.log(a);
+// console.log(c);
+
+// Operador Spread
+// const numeros = [1, 2, 3];
+// const masNumeros = [...numeros, 4, 5, ...numeros, ...[50, 60]];
+// console.log(masNumeros);
+
+function sumar(...numeros) {
+  let resultado = 0;
+  for(let numero of numeros) {
+    resultado += numero;
+  }
+  return resultado;
+}
+
+//console.log(sumar(1, 3, 8, 9, 5, 10, 21));
+const numeros = [1, 3, 8, 9, 5, 10, 21];
+console.log(sumar(...numeros));
